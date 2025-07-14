b558d12b4b29a35147e95f7985eeb080ec3bb8ce
DESKTOP-RBNO6LS\SSAFY <f.potato.potage@gmail.com>
2025-07-14 17:03:05 +0900

    0714_2

--- BOJ/1913.py
+++ BOJ/1913.py
@@ -1,4 +1,52 @@
 """
+홀수인 자연수 N이 주어지면, 다음과 같이 1부터 N ** 2 까지의 자연수를 달팽이 모양으로 N * N의 표에 채울 수 있다
+
+위 오른쪽 아래 왼쪽
+
+"""
+import sys
+sys.stdin = open('1913.txt', 'r')
+
+N = int(input())
+target = int(input())
+
+arr = [[0] * N for _ in range(N)]
+
+# 거꾸로 맨 뒤에서부터
+direction = {
+    0: [1, 0],
+    1: [0, 1],
+    2: [-1, 0],
+    3: [0, -1],
+}
+
+num = N * N
+curr_dir = 0
+curr_r, curr_c = 0, 0
+
+
+arr[0][0] = num
+num -= 1
+
+while num >= 1:
+    nr, nc = curr_r + direction[curr_dir][0], curr_c + direction[curr_dir][1]
+
+    if 0 <= nr < N and 0 <= nc < N and arr[nr][nc] == 0:
+        arr[nr][nc] = num
+        if num == target:
+            target_r = nr + 1
+            target_c = nc + 1
+
+        num -= 1
+        curr_r, curr_c = nr, nc
+    else:
+        curr_dir = (curr_dir + 1) % 4
+
+for row in arr:
+    print(*row)
+print(target_r, target_c)
+
+"""
 49 26 27 28 29 30 31
 48 25 10 11 12 13 32
 47 24 9 2 3 14 33
